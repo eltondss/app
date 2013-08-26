702e6764ba0f1c0780ce3d0f1a98c4ff05cc8a3d
Elton <eltonschmoeller@gmail.com>
2013-08-26 11:12:21 -0300

    Commit inicial

---
+++ App/src/com/betha/beans/InicioBean.java
@@ -0,0 +1,16 @@
+package com.betha.beans;
+
+public class InicioBean {
+private String name;
+
+public void enviar(){
+	
+}
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+}
